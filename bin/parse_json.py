a1a6868c3b6919ed812ea7f6a9c345376f72f3a9
Chelsea <chelseakarasawyer@gmail.com>
2019-03-14 16:08:56 +0000

    py script changes
    -Fixed error in json script

--- bin/parse_json.py
+++ bin/parse_json.py
@@ -31,10 +31,12 @@ with open(samplesheet, 'r') as f:
     for idx, row in enumerate(reader):
         if data_tag_search in row:
             data_index = idx
+
 sample_pd = pd.read_csv(samplesheet, skiprows=range(0, data_index + 1))
 
 # slice sample sheet for only problem rows
 SS_new_problem_ids = sample_pd.loc[sample_pd['Sample_ID'].isin(problem_samples_list)].copy()
+prob_lanes = SS_new_problem_ids.Lane.unique()
 
 # create new column for read counts
 SS_new_problem_ids['read_count'] = 0
@@ -43,44 +45,47 @@ SS_new_problem_ids['read_count'] = 0
 json_file = json_file["UnknownBarcodes"]
 
 list_dict_matches = []
-for index, row in SS_new_problem_ids.iterrows():
-    for result in json_file:
-        if result['Lane'] == row['Lane']:
-            for unknown_idx, unknown_count in result['Barcodes'].items():
-                # dual indexed lane
-                if "+" in unknown_idx:
-                    indexes = unknown_idx.split("+")
-                    index1= indexes[0]
-                    index2 = indexes[1]
-                    # find indexes that are single indexes on dual lanes
-                    if index1 == row['index'] and pd.isnull(row['index2']) is True:
-                        # ensure its the highest read count of the matches
-                        if unknown_count > row['read_count']:
-                            sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index2'] = index2
-                            SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
-                    # find partial matches for short index 1 that is dual indexed
-                    elif index1.startswith(row['index']) is True and index2 == row['index2']:
-                        if unknown_count > row['read_count']:
-                            sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index'] = index1
-                            SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
-                    # find partial matches for short index 1 that is single indexed
-                    elif index1.startswith(row['index']) is True and pd.isnull(row['index2']) is True:
-                        if unknown_count > row['read_count']:
-                            sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index2'] = index2
-                            sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index'] = index1
-                            SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
-                    # find partial matches for short index 2 that is dual indexed
-                    elif index2.startswith(row['index2']) is True and index1 == row['index']:
-                        if unknown_count > row['read_count']:
-                            sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index2'] = index2
-                            SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
-                # single indexed lane
-                elif "+" not in unknown_idx:
-                    # find partial matches for short index 1 that is single indexed
-                    if unknown_idx.startswith(row['index']) is True and pd.isnull(row['index2']) is True:
-                        if unknown_count > row['read_count']:
-                            sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index'] = unknown_idx
-                            SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
+
+for result in json_file:
+    if result['Lane'] in prob_lanes:
+        for unknown_idx, unknown_count in result['Barcodes'].items():
+            for index, row in SS_new_problem_ids.iterrows():
+                if result['Lane'] == row['Lane']:
+                    # dual indexed lane
+                    if "+" in unknown_idx:
+                        indexes = unknown_idx.split("+")
+                        index1= indexes[0]
+                        index2 = indexes[1]
+                        # find indexes that are single indexes on dual lanes
+                        if index1 == row['index'] and pd.isnull(row['index2']) is True:
+                            # ensure its the highest read count of the matches
+                            if unknown_count > row['read_count']:
+                                sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index2'] = index2
+                                SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
+                        # find partial matches for short index 1 that is dual indexed
+                        elif index1.startswith(row['index']) is True and index2 == row['index2']:
+                            if unknown_count > row['read_count']:
+                                sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index'] = index1
+                                SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
+                        # find partial matches for short index 1 that is single indexed
+                        elif index1.startswith(row['index']) is True and pd.isnull(row['index2']) is True:
+                            if unknown_count > row['read_count']:
+                                sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index2'] = index2
+                                sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index'] = index1
+                                SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
+                        # find partial matches for short index 2 that is dual indexed
+                        elif index2.startswith(row['index2']) is True and index1 == row['index']:
+                            if unknown_count > row['read_count']:
+                                sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index2'] = index2
+                                SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
+                    # single indexed lane
+                    elif "+" not in unknown_idx:
+                        # find partial matches for short index 1 that is single indexed
+                        if unknown_idx.startswith(row['index']) is True and pd.isnull(row['index2']) is True:
+                            if unknown_count > row['read_count']:
+                                sample_pd.loc[sample_pd['Sample_ID'] == row['Sample_ID'], 'index'] = unknown_idx
+                                SS_new_problem_ids.loc[SS_new_problem_ids['Sample_ID'] == row['Sample_ID'], 'read_count'] = unknown_count
+
 
 # delete read count column from dataframe
 SS_new_problem_ids.drop('read_count', 1, inplace=True)
