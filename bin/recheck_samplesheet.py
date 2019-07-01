80e40c87ef332102644281ad5c4e54f90e9d037b
Chelsea <chelseakarasawyer@gmail.com>
2019-07-01 13:47:46 +0100

    Update recheck_samplesheet.py

--- bin/recheck_samplesheet.py
+++ bin/recheck_samplesheet.py
@@ -43,14 +43,13 @@ sample_pd = pd.read_csv(samplesheet, skiprows=range(0, ss_idx + 1))
 new_ss_idx = getdatatag(newsamplesheet)
 newsample_pd = pd.read_csv(newsamplesheet, skiprows=range(0, new_ss_idx + 1))
 SS_new_problem_ids = newsample_pd.iloc[problem_samples_list]
-
 test_result = 'pass'
 
 # compare problem sample rows and if the same return fail
 for index, row in SS_new_problem_ids.iterrows():
-    update_idx_val =sample_pd.iloc[index]
-    if update_idx_val['Sample_ID'] == row['Sample_ID'] and update_idx_val['index'] == row['index'] and update_idx_val['Lane'] == row['Lane']:
-            if update_idx_val['index2'] == row['index2'] or (pd.isna(update_idx_val['index2']) and pd.isna(row['index2'])):
+    update_idx_val = sample_pd.loc[(sample_pd['Sample_ID'] == row['Sample_ID']) & (sample_pd['Lane'] == row['Lane'])]
+    if update_idx_val['index'].item() == row['index']:
+            if update_idx_val['index2'].item() == row['index2'] or (pd.isna(update_idx_val['index2'].item()) and pd.isna(row['index2'])):
                 test_result = 'fail'
 
 x = open(test_result + ".txt", "w")
